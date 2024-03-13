3ef45adcd67eab28cbdde0efac3fc3acaccfd94a
Peter <pengu20@student.sdu.dk>
2024-03-13 15:09:43 +0100

    Added the robot dynamics part

---
+++ UR_NULL_BOT_dynamics/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/peter/OneDrive/Skrivebord/Uni stuff/2. Masters/2. masters_OBSIDIAN/robot Project/ur-dynamics-cpp-master
+# Build directory: /home/peter/OneDrive/Skrivebord/Uni stuff/2. Masters/2. masters_OBSIDIAN/robot Project/ur-dynamics-cpp-master/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("pybind11")
