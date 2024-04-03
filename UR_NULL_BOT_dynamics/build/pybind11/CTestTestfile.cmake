6b19226cc37fd06c471c099fb56e0964a237aa27
Peter <pengu20@student.sdu.dk>
2024-04-03 11:40:48 +0200

    Removed emils dynamics model

--- UR_NULL_BOT_dynamics/build/pybind11/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/peter/OneDrive/Skrivebord/Uni stuff/2. Masters/2. masters_OBSIDIAN/robot Project/ur-dynamics-cpp-master/pybind11
-# Build directory: /home/peter/OneDrive/Skrivebord/Uni stuff/2. Masters/2. masters_OBSIDIAN/robot Project/ur-dynamics-cpp-master/build/pybind11
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- UR_NULL_BOT_dynamics/build/temp.linux-x86_64-3.10/ur_robot/pybind11/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/peter/OneDrive/Skrivebord/Uni stuff/2. Masters/2. masters_OBSIDIAN/robot Project/null_snake/UR_NULL_BOT_dynamics/pybind11
-# Build directory: /home/peter/OneDrive/Skrivebord/Uni stuff/2. Masters/2. masters_OBSIDIAN/robot Project/null_snake/UR_NULL_BOT_dynamics/build/temp.linux-x86_64-3.10/ur_robot/pybind11
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
